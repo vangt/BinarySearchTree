a19384cbcb94923607a0a473e63588396b5c7314
vangt <vangt86@gmail.com>
2017-08-23 08:05:00 -0500

    Created Left and Right children properties and constructor.

--- BinarySearchTree/Classes/Node.cs
+++ BinarySearchTree/Classes/Node.cs
@@ -8,5 +8,37 @@ namespace BinarySearchTree.Classes
 {
     public class Node
     {
+        private Node leftChild;
+        private Node rightChild;
+        public int number;
+
+        public Node()
+        {
+            this.number = number;
+        }
+
+        public Node LeftChild
+        {
+            get
+            {
+                return leftChild;
+            }
+            set
+            {
+                this.leftChild = value;
+            }
+        }
+
+        public Node RightChild
+        {
+            get
+            {
+                return rightChild;
+            }
+            set
+            {
+                this.rightChild = value;
+            }
+        }
     }
 }
